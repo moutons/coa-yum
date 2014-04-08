96413913340e95bc04a5d23d81a31a7ca1a50089
Jerald Sheets <questy@gmail.com>
2014-04-08 14:24:56 -0500

    Adding chain here instead of install module

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,10 +3,12 @@ class coa-yum::config {
   exec { 'importkey':
     command => '/bin/rpm --import http://dl.iuscommunity.org/pub/ius/IUS-COMMUNITY-GPG-KEY',
     path    => '/bin',
+    before  => Class[ 'coa-yum::install'],
   }
 
   exec { 'installepel':
     command => '/bin/rpm -Uvh http://mirror.pnl.gov/epel/6/i386/epel-release-6-8.noarch.rpm',
     path    => '/bin',
+    before  => Class[ 'coa-yum::install'],
   }
 }
